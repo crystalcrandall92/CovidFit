b07f3fef291768fa4afc35833b42aa53b2f180ae
Fabian Byrd <fabianbyrd@Fabosmac.attlocal.net>
2021-01-06 20:39:35 -0500

    fixed background

--- client/src/pages/Signin.js
+++ client/src/pages/Signin.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import Header from "../components/Header/Header";
 import Nav from "../components/Nav/Nav";
+import "../components/css/styles.css"
 
 // setting state to value and books
 class Signin extends Component {
 
   render() {
     return (
-      <div>
+      <div className="signinbg">
         <Nav />
         <Header />
       </div>
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import Header from "../components/Header/Header";
 import Nav from "../components/Nav/Nav";
+import "../components/css/styles.css";
 
 // setting state to value and books
 class Signup extends Component {
 
   render() {
     return (
-      <div>
+      <div className="signupbg">
         <Nav />
         <Header />
       </div>
