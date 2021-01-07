9ce7390b1fe67162587655e66889c19a4a58b046
Shelby <shelbypkelly@gmail.com>
2021-01-06 19:15:27 -0500

    padding on styles.css, working on having saved items pushed to fitness page

--- client/src/pages/Fitness.js
+++ client/src/pages/Fitness.js
@@ -2,14 +2,26 @@ import "../components/css/styles.css";
 import React, { Component } from "react";
 import Header from "../components/Header/Header";
 import Nav from "../components/Nav/Nav";
+import API from "../utils/API";
+//import SearchContainer from "../components/SearchContainer";
 
 class Fitness extends Component {
- 
+  state = {
+    savedList: []
+  }
+  
+  componentDidMount() {
+    API.savedList()
+    .then(savedList => this.setState({ savedList: savedList }))
+  }
+  
   render() {
     return (
       <div>
-        <Nav />
-        <Header />
+      <Nav />
+      <Header />
+      <h1>Your saved items to calorie count.</h1>
+      
       </div>
     )
   }
