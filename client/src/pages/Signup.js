e39b68b8d22fc6e8d4a1a9f8478f8365390a32a2
Fabian Byrd <fabianbyrd@Fabosmac.attlocal.net>
2021-01-09 14:22:18 -0500

    added signin/signup forms

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,71 +1,25 @@
 import React, { Component } from "react";
 import Header from "../components/Header/Header";
 import Nav from "../components/Nav/Nav";
-import "../components/css/styles.css";
+import SignupField from "../components/SignupField/SignupField";
+import "../components/css/styles.css"
 
-// sign up form behaves almost identically to log in form. We could create a flexible Form component to use for both actions, but for now we'll separate the two:
-class SignUp extends React.Component {
-	state = {
-		fields: { name: '', email: '', password: ''}
-	}
 
-	onInputChange(evt) {
-		this.setState({
-			fields: {
-				...this.state.fields,
-				[evt.target.name]: evt.target.value
-			}
-		})
-	}
-
-//	onFormSubmit(evt) {
-//		evt.preventDefault()
-//		API.signUp(this.state.fields).then(user => {
-//			this.setState({ fields: { name: '', email: '', password: '' } })
-//			if(user) {
-//				this.props.onSignUpSuccess(user)
-//				this.props.history.push('/')
-//			}
-//		})
-//	}
-	
-	render() {
-		const { name, email, password } = this.state.fields
-		return (
-			<div className='signupbg'>
+// setting state to value and books
+class Signup extends Component {
+  
+  render() {
+    return (
+      <div className="signupbg">
         <Nav />
-          <Header />
-				<div className='row'>
-					<div className='column column-33 column-offset-33'>
-						<h1>Sign Up</h1>
-						<form>
-							<input 
-								type="text" 
-								placeholder="Name" 
-								name="name" 
-								value={name}
-							/>
-							<input 
-								type="text" 
-								placeholder="Email" 
-								name="email" 
-								value={email} 
-								onChange={this.onInputChange.bind(this)}
-							/>
-							<input 
-								type="password" 
-								placeholder="Password" 
-								name="password" 
-								value={password} 
-								onChange={this.onInputChange.bind(this)}
-							/>
-							<button>Sign Up</button>
-						</form>
-					</div>
-				</div>
-			</div>
-		)
-	}
+        <Header />
+        <SignupField />
+
+      </div>
+    );
+  }
 }
 
-export default SignUp;
+
+
+export default Signup;
