d606058c97f20c4856a8163a644e41928fb0144f
Crystal Crandall <Crystalcrandall92@gmail.com>
2021-01-10 23:48:05 -0500

    fixing total.js to render total calories, adding start of auth

--- client/src/components/Total/Total.js
+++ client/src/components/Total/Total.js
@@ -1,12 +1,9 @@
 import React from "react";
 
 const Total = (props) => {
-    const string = JSON.stringify(props.savedFoods)
-const total = string.reduce(
-    (previousScore, currentValue) => previousScore + currentValue.Calories, 0
+const total = props.savedFoods.reduce(
+    (previousScore, currentValue) => parseInt(previousScore) + parseInt(currentValue.Calories), 0
 );
-
-console.log(string)
 
     return (
         <p>Totals Calories: {total}</p>
